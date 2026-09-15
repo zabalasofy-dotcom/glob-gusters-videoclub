2f4de06ee5cddba286232da4600a01803a4c0115
alisonsuarez <asuarezv@ucompensar.edu.co>
2026-09-15 12:26:48 -0500

    Add LICENSE and README files; create initial SQL script for database setup..

---
+++ sql/glob_gusters.sql
@@ -0,0 +1,10 @@
+-- Base de Datos Relacional Glob-Gusters Video-Club
+-- Autor: Andrés Felipe Gutiérrez Rivera
+-- Fecha: 2024-06-10
+
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `glob_gusters`;
+
+-- Eliminación de la base de datos
+-- DROP DATABASE IF EXISTS `glob_gusters`;
