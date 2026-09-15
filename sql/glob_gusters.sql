76b338d102730ecf8eede538c46de04aed97f46d
Allison <zabalasofy@gmail.com>
2026-09-15 12:56:20 -0500

    Create initial SQL tables for Glob-Gusters Video-Club database

--- sql/glob_gusters.sql
+++ sql/glob_gusters.sql
@@ -8,3 +8,78 @@ USE `glob_gusters`;
 
 -- Eliminación de la base de datos
 -- DROP DATABASE IF EXISTS `glob_gusters`;
+
+CREATE TABLE Actor(
+    ID INTEGER PRIMARY KEY;
+    Nacionalidad_ID INTEGER Foreign Key;
+    Nombre VARCHAR;
+    Sexo CHAR;
+)
+
+CREATE TABLE Cliente(
+    DNI INTEGER PRIMARY KEY;
+    Cliente_DNI INTEGER Foreign Key;
+    Nombre VARCHAR;
+    Direccion VARCHAR;
+    Telefono VARCHAR;
+)
+
+CREATE TABLE Nacionalidad(
+   ID INTEGER PRIMARY KEY;
+   Nombre VARCHAR;
+)
+
+CREATE TABLE Reparto(
+   Actor_ID INTEGER Foreign Key;
+   Pelicula_ID INTEGER Foreign Key;
+   Rol INTEGER;
+)
+
+CREATE TABLE Renta(
+   Numero INTEGER PRIMARY KEY;
+   Cliente_DNI INTEGER Foreign Key;
+   Inicia DATE;
+   Finaliza DATE;
+)
+
+CREATE TABLE Ejemplar_Renta(
+    Ejemplar_Numero INTEGER Foreign Key;
+    Renta_Numero INTEGER Foreign Key;
+    Entrega DATE;
+)
+
+CREATE TABLE Reparto(
+    ID INTEGER PRIMARY KEY;
+    Nacionalidad_ID INTEGER Foreign Key;
+    Nombre VARCHAR;
+)
+
+CREATE TABLE Director(
+    ID INTEGER PRIMARY KEY;
+    Nacionalidad_ID INTEGER Foreign Key;
+    Nombre VARCHAR;
+)
+
+CREATE TABLE Pelicula(
+    ID INTEGER PRIMARY KEY;
+    Productora_ID INTEGER Foreign Key;
+    Titulo VARCHAR;
+    Director_ID INTEGER Foreign Key;
+    Fecha DATE;
+)
+
+CREATE TABLE Ejemplar(
+   Numero INTEGER PRIMARY KEY;
+   Estado_ID INTEGER Foreign Key;
+   Pelicula_ID INTEGER Foreign Key;
+)
+
+CREATE TABLE Estado(
+   ID INTEGER PRIMARY KEY;
+    Nombre VARCHAR;
+)
+
+CREATE TABLE Productora(
+   ID INTEGER PRIMARY KEY;
+   Nombre VARCHAR;
+)
