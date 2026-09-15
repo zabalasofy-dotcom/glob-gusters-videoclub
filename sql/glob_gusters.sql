6ebdea137b5de40454d48a013a0509a49787738b
Allison <zabalasofy@gmail.com>
2026-09-15 11:59:25 -0500

    Add LICENSE and README files; create initial SQL script for Glob-Gusters Video-Club database

---
+++ sql/glob_gusters.sql
@@ -0,0 +1,10 @@
+-- Base de Datos Relacional Glob-Gusters Video-Club
+-- Autor: Allison Sofia Zabala Serrano
+-- Fecha: 2024-09-15
+
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `glob_gusters`;
+
+-- Eliminación de la base de datos
+-- DROP DATABASE IF EXISTS `glob_gusters`;
